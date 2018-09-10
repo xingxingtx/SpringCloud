00cfff1ad89ea281a573f749944678dba77436dd
wei.peng <wei.peng@aorise.org>
2018-09-10 15:04:21 +0800

    springcloud

---
+++ eureka-server-two/src/test/java/eurekaservertwo/EurekaServerTwoApplicationTests.java
@@ -0,0 +1,16 @@
+package eurekaservertwo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerTwoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
